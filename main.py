420e6285289889c08255cb7e6e1f78ae4270fe7e
hugoj78 <hugoj.egu@gmail.com>
2022-03-17 10:00:39 +0100

    init

--- main.py
+++ main.py
@@ -36,8 +36,28 @@ def read_item(id: str):
 	else :
 		return {"no Json"}
 
-
 @app.get("/museum/position/{lon}/{lat}/{dist}")
 def read_museum_by_position(lon: str, lat: str, dist: str):
 	r = requests.get(f'https://opendata.paris.fr/api/records/1.0/search/?dataset=que-faire-a-paris-&q=&facet=date_start&facet=date_end&facet=tags&facet=address_name&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=transport&facet=price_type&facet=access_type&facet=updated_at&facet=programs&geofilter.distance={lon},{lat},{dist}')
 	return r.json()
+
+
+
+
+
+from pymongo import MongoClient
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("mongodb://admin:admin@clusterynov-shard-00-00.pipqs.mongodb.net:27017,clusterynov-shard-00-01.pipqs.mongodb.net:27017,clusterynov-shard-00-02.pipqs.mongodb.net:27017/ServiceWeb?ssl=true&replicaSet=atlas-fekj21-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.ServiceWeb
+col = db.Data
+# Issue the serverStatus command and print the results
+#serverStatusResult=db.command("serverStatus")
+#pprint(serverStatusResult)
+
+lists = col.find()
+print(lists)
+
+for doc in lists:
+	print(doc)
